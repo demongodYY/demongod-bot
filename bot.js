cc36618afda5111ec579e9d5322eeed120bddd29
qq yy <qq yy>
2017-11-13 22:04:50 +0800

    test mutil waterfall

--- bot.js
+++ bot.js
@@ -5,33 +5,33 @@ const bot = new builder.UniversalBot(connector, [
     session => {
         // session.send("hello! %s", session.message.text);
         session.send("欢迎来到观影体验");
-        builder.Prompts.time(session, "您需要什么时候预定？");
-    },
-    (session, results) =>{
-        session.dialogData.reservationTime = builder.EntityRecognizer.resolveTime([results.response]);
-        builder.Prompts.text(session, "您需要预定几人的包间？");
+        session.beginDialog('reservation', session.dialogData.reservation);
     },
     (session, results) => {
-        session.dialogData.peopleNum = results.response;
-        builder.Prompts.text(session, "请问怎么称呼您？");
-    },
-    (session, results) => {
-        session.dialogData.userName = results.response;
-        session.send(`您好，${session.dialogData.userName}, 已成功为您预订了 ${session.dialogData.reservationTime} 的 ${session.dialogData.peopleNum}人 包间`);
+        session.dialogData.reservation = results.response;
+        session.send(`您好，${session.dialogData.reservation.userName}, 已成功为您预订了 ${session.dialogData.reservation.time} 的 ${session.dialogData.reservation.peopleNum}人 包间`);
         session.endDialog();
     }
 ])
 
-// bot.dialog('test', [
-//     (session) => {
-//         builder.Prompt.text(session, 'TEST!!!');
-//     },
-//     (session, results) => {
-//         session.endDialog(`hello!${results.response}`);
-//     }
+bot.dialog('reservation', [
+    (session) => {
+        builder.Prompts.time(session, "您需要什么时候预定？");
+    },
+    (session, results) =>{
+        session.dialogData.reservation.time = builder.EntityRecognizer.resolveTime([results.response]);
+        builder.Prompts.text(session, "您需要预定几人的包间？");
+    },
+    (session, results) => {
+        session.dialogData.reservation.peopleNum = results.response;
+        builder.Prompts.text(session, "请问怎么称呼您？");
+    },
+    (session, results) => {
+        session.dialogData.reservation.userName = results.response;
+        session.endDialogWithResult({response:session.dialogData.reservation});
+    }
 
-
-// ]);
+]);
 
 
 module.exports = connector;
